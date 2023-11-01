4d30cee5f7959a5ce57d7cb7e2c3ef19fb93b424
50h100a <all_awful@proton.me>
2023-11-01 00:53:02 +0000

    ngram, but it doesn't do what I want it to

--- aphrodite/common/logits_processor.py
+++ aphrodite/common/logits_processor.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import torch
+from collections import defaultdict, Counter
 
 
 class LogitsProcessor(ABC):
@@ -66,3 +67,66 @@ class BanTokens(LogitsProcessor):
 
     def __call__(self, logits: torch.Tensor, output_tokens: list[list[int]]) -> None:
         logits[:, self._banned_token_ids] = -float("inf")
+
+
+class NGramPenaltyProcessor(LogitsProcessor):
+    """Use ngrams to apply repetition penalties to token sequences."""
+    def __init__(self, penalty:float, prompt:list[int],
+                 immune_sequences:list[list[int]] = [],
+                 n_min:int=2, n_max:int=4, tokenizer=None):
+        self._penalty = penalty
+        self._n_min = n_min
+        self._n_max = n_max
+        self._tail = prompt[-n_max:]
+
+        # What if, instead, I use word ngrams? That seems slightly insane.
+        
+        prompt_mask = [True] * len(prompt)  # If False, token is part of an ignored sequence, and should not be included in any penalties?
+        for seq in immune_sequences:
+            sidx = 0
+            i = 0
+            while i < len(prompt):
+                if seq[sidx] == prompt[i]:
+                    sidx += 1
+                    if sidx == len(seq):
+                        prompt_mask[i-len(seq):i] = False
+                        i -= len(seq)  # Rewind so we can catch self-overlapping sequences.
+                        sidx = 0
+                else:
+                    sidx = 0
+                i += 1
+
+        # I'm not sure that's what we want. We DO want penalties for starting every reply the exact same way.
+        # What we DON'T want is penalties on the instruct sequence _itself_.
+        #   Does that mean the 'weight' is moved to the other tokens in those sequences? Or just not counted at all?
+        #Ahh, hm. We want to nullify the penalty only when RESULTING TOKENS are part of the protected sequences.
+        #   Protected sequences can still *contribute* to penalties for other tokens.
+
+        penalties = defaultdict(lambda:Counter())  # seq -> penalized id -> penalty
+        top = Counter()
+        for ng in range(n_min,n_max):
+            for i in range(ng, len(prompt)):
+                geh = tuple(prompt[i-ng:i])
+                if prompt_mask[i]:
+                    penalties[geh].update([prompt[i]])
+                    top.update([tuple(prompt[i-ng:i+1])])
+        print(f"created {len(penalties)} ngrams jfc, {top.total()} instances")
+        print(f"Immunes:", immune_sequences)
+        print('\n'.join([f"{tokenizer.convert_ids_to_tokens(x[0])}: {x[1]} ({self._penalty ** (x[1] / (self._n_max - self._n_min)):.02f})" for x in top.most_common()[:10]]))
+        self._penalties = dict(penalties)
+
+    def __call__(self, logits: torch.Tensor, output_tokens: list[list[int]]) -> None:
+        for i,outputs in enumerate(output_tokens):
+            # print("b4", logits.sort(dim=1, descending=True)[0][0,:10].tolist())
+            tail = (self._tail + outputs)[-self._n_max:]
+            for n in range(self._n_min, self._n_max):
+                key = tuple(tail[-n:])
+                if key in self._penalties:
+                    for tok,pen in self._penalties[key].items():
+                        pval = self._penalty ** (pen / (self._n_max - self._n_min))
+                        logits[i,tok] /= pval if logits[i,tok] > 0 else 1/pval
+            # print("AF", logits.sort(dim=1, descending=True)[0][0,:10].tolist())
+        # logits[:, self._banned_token_ids] = -float("inf")
+
+
+
