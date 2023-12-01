71f334c497fec08a3e3a3e7e68abdc7cf88bf926
50h100a <all_awful@proton.me>
2023-12-01 22:49:47 +0000

    print expected concurrency on startup

--- aphrodite/engine/aphrodite_engine.py
+++ aphrodite/engine/aphrodite_engine.py
@@ -205,6 +205,7 @@ class AphroditeEngine:
         # FIXME: Change to debug log.
         logger.info(f"# GPU blocks: {num_gpu_blocks}, "
                     f"# CPU blocks: {num_cpu_blocks}")
+        logger.info(f"Expected concurrency: {num_gpu_blocks * self.cache_config.block_size / self.scheduler_config.max_model_len:.02f}x")
 
         if num_gpu_blocks <= 0:
             raise ValueError("No available memory for the cache blocks. "
