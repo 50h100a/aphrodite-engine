2ac32cae78c0783befb75b1fae9eba4f442f1833
50h100a <all_awful@proton.me>
2024-11-09 02:30:08 +0000

    do not cache the wheel-building docker image

--- build_wheel.sh
+++ build_wheel.sh
@@ -5,7 +5,7 @@ MAX_JOBS=${MAX_JOBS:-}
 NVCC_THREADS=${NVCC_THREADS:-}
 TORCH_CUDA_ARCH_LIST="6.0 6.1 7.0 7.5 8.0 8.6 8.9 9.0+PTX"
 
-DOCKER_BUILDKIT=1 docker build . --target build --tag alpindale/aphrodite-build \
+DOCKER_BUILDKIT=1 docker build . --target build --tag alpindale/aphrodite-build --no-cache\
     --build-arg CUDA_VERSION=$CUDA_VERSION \
     ${MAX_JOBS:+--build-arg max_jobs=$MAX_JOBS} \
     ${NVCC_THREADS:+--build-arg nvcc_threads=$NVCC_THREADS}
